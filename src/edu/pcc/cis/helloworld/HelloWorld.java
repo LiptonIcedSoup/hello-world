7be2a2772dca57188fee1f016d255a7354188545
Steven Williford <steven.williford@pcc.edu>
2022-05-02 17:45:37 -0700

    Added Author name

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,9 @@
 package edu.pcc.cis.helloworld;
 import java.util.Date;
+
+/**
+ * Author Steven T Williford
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
